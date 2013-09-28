12a131e0b5b5ed203aa02eb08459424fb6100bf0
shany <shanyyaar@gmail.com>
2013-09-28 12:05:32 +0530

    add a full_title helper

--- Gemfile
+++ Gemfile
@@ -4,11 +4,13 @@ ruby '1.9.3'
 
 gem 'rails', '3.2.7'
 
+
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 group :development do
-gem 'sqlite3' , '1.3.5'
+gem 'sqlite3', '1.3.5'
 gem 'rspec-rails' , '2.10.0'
+gem 'guard-rspec' , '0.5.5'
 end
 
 
@@ -42,7 +44,12 @@ gem 'jquery-rails' , '2.0.3'
 # gem 'debugger'
 group :test do
 gem 'capybara', '1.1.2'
+gem 'rb-inotify', '0.8.8'
+gem 'libnotify', '0.5.9'
+gem 'guard-spork', '0.3.2'
+gem 'spork', '0.9.0'
 end
+
 group :production do
-gem 'pg', '0.12.2'
+	gem 'pg', '0.12.2'
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,12 +10,17 @@ describe "Static Pages" do
       page.should have_selector('h1', :text => 'Sample App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
     end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
   end
+end
 
   describe "Help page" do
 
@@ -26,7 +31,7 @@ describe "Static Pages" do
 
   	it "should have the title 'Help'" do
        visit '/static_pages/help'
-       page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+       page.should have_selector('title', :text => " | Help")
   end
 end
 
@@ -39,7 +44,22 @@ end
 
     it "should have the title 'About Us'" do
        visit '/static_pages/about'
-       page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
+       page.should have_selector('title', :text => " | About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "| Contact")
     end
   end
 end
